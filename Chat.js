19189f4185f44c8ca979df3708106285797ff893
seungho Jeon <urijsh612@gmail.com>
2019-12-30 20:36:50 +0900

    #21.3 Sending Messages and KeyboardAvoidingView

--- Chat.js
+++ Chat.js
@@ -1,10 +1,16 @@
-import React from "react";
-import { View, Text, ScrollView, TextInput } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  TextInput,
+  KeyboardAvoidingView
+} from "react-native";
 import gql from "graphql-tag";
-import { useQuery } from "react-apollo-hooks";
+import { useQuery, useMutation } from "react-apollo-hooks";
 import withSuspense from "./withSuspense";
 
-const CHAT = gql`
+const GET_MESSAGE = gql`
   query messages {
     messages {
       id
@@ -13,20 +19,74 @@ const CHAT = gql`
   }
 `;
 
+const SEND_MESSAGE = gql`
+  mutation sendMessage($text: String!) {
+    sendMessage(text: $text) {
+      id
+      text
+    }
+  }
+`;
+
 const Chat = () => {
-  const { data, error } = useQuery(CHAT, { suspend: true });
-  console.log(data);
+  const [message, setMessage] = useState("");
+  const [sendMessagesMutation] = useMutation(SEND_MESSAGE, {
+    variables: {
+      text: message
+    }
+  });
+
+  const { data, error } = useQuery(GET_MESSAGE, { suspend: true });
+
+  const onChangeText = text => setMessage(text);
+
+  const onSubmit = async () => {
+    console.log("ok");
+    if (message === "") {
+      console.log(1);
+      return;
+    }
+    try {
+      console.log(2);
+      await sendMessagesMutation();
+      setMessage("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <ScrollView
-      contentContainerStyle={{
-        paddingVertical: 50,
-        flex: 1,
-        justifyContent: "flex-end",
-        alignItems: "center"
-      }}
-    >
-      <Text>Hello</Text>
-    </ScrollView>
+    <KeyboardAvoidingView style={{ flex: 1 }} enabled behavior="padding">
+      <ScrollView
+        contentContainerStyle={{
+          paddingVertical: 50,
+          flex: 1,
+          justifyContent: "flex-end",
+          alignItems: "center"
+        }}
+      >
+        {data.messages.map(m => (
+          <View key={m.id} style={{ marginBottom: 10 }}>
+            <Text>{m.text}</Text>
+          </View>
+        ))}
+        <TextInput
+          placeholder="Type a message"
+          style={{
+            marginTop: 50,
+            width: "90%",
+            borderRadius: 10,
+            paddingVertical: 15,
+            paddingHorizontal: 10,
+            backgroundColor: "#f2f2f2"
+          }}
+          returnKeyType="send"
+          value={message}
+          onChangeText={onChangeText}
+          onSubmitEditing={onSubmit}
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
--- withSuspense.js
+++ withSuspense.js
@@ -1,5 +1,7 @@
 // Suspnese 담당 파일
 // Suspnese: use <Suspense> to declaratively “wait” for anything else, including data
+// 즉, 데이터 받아올 동안 실행될 컴포너는트를 지정 할 수 있다.
+// 이것은 useQuery 등 사용시 loading을 사용한 로딩 처리를 대체 할 수 있다.
 
 import React, { Suspense } from "react";
 import { View, ActivityIndicator } from "react-native";
