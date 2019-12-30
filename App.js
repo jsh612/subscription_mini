8d19f050908f0b6bb6e6590aa85ca8b6a10a9baf
seungho Jeon <urijsh612@gmail.com>
2019-12-30 22:34:09 +0900

    #21.5 Push Notifications

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import React, { Suspense } from "react";
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import React, { useState, useEffect } from "react";
+import { StyleSheet } from "react-native";
 import { ApolloProvider } from "react-apollo-hooks";
+import { Notifications } from "expo";
+import * as Permissions from "expo-permissions";
 import client from "./apollo";
 import Chat from "./Chat";
 
 export default function App() {
+  const [notifi, setNotifi] = useState(false);
+
+  const ask = async () => {
+    const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+    setNotifi(status);
+    console.log(1);
+    // # 토큰
+    //  - 서버에 저장하여, 특정 디바이스를 식별하는데 사용한다.
+    //  -https://docs.expo.io/versions/v36.0.0/guides/push-notifications/#1-save-the-users-expo-push-token
+    let token = await Notifications.getExpoPushTokenAsync();
+    console.log(token);
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     <ApolloProvider client={client}>
       <Chat />
